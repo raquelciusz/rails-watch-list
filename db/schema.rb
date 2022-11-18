aa6c692211940c397f19f72e3a0b74cc3d2c6a84
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-11-18 16:31:43 -0300

    add dotenv-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_164509) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_163702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_164509) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "list_id", null: false
     t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
